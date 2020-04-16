3d24539c539db2f37af4f53caa93127808cc42d7
Michael Ross <mross.j@gmail.com>
2020-04-15 20:12:46 -0400

    update knexfile.js
    * updated dependencies
    * updated server.js
    * added filename, seeds, migrations, and pool to knexfile.js
    * added migration table
    * added base seed data

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -4,4 +4,11 @@ const knexfile = require('../knexfile.js');
 const db = knex(knexfile.development);
 const router = express.Router();
 
+
+
+
+
+
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const carsRouter = require('./cars/carsRouter.js');
 const server = express().use(express.json());
+const helmet = require('helmet');
+const cors = require('cors');
 
+server.use(helmet());
+server.use(cors());
 server.use('/api/cars', carsRouter);
 
 server.get('/', (req, res) => {
